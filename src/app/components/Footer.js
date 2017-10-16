653c68d13dff0ef38823b69de14ed11f0b0b015e
Nitesh <niteshp27@gmail.com>
2017-10-16 13:31:12 +0530

    added footer file

---
+++ src/app/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Footer extends React.Component{
+    render(){
+        return(
+            <footer>
+                <div className="container-wrapper">
+                    <p>This is footer:- </p>
+                </div>
+            </footer>
+        );
+    }
+}
