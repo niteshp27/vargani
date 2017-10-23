ca603aa378990f098ecf94268ac45e571e31181b
Nitesh <niteshp27@gmail.com>
2017-10-23 17:17:36 +0530

    converted class to constant for header component

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -6,7 +6,24 @@ import {
     MenuDivider
 } from "@blueprintjs/core";
 
-
+export const Header = (props) => {
+    console.log("Indside header.js");
+    const menu = (
+        <Menu>
+            <MenuItem text="New" />
+            <MenuItem text="Open" />
+            <MenuItem text="Save" />
+        </Menu>
+    );
+    return (
+        <header className="header">      
+            <div className="container-wrapper">
+                {menu}
+            </div>
+        </header>
+    );
+};
+/*
 export class Header extends React.Component{
     shouldComponentUpdate(nextProps, nextState) {
         return false;
@@ -30,3 +47,5 @@ export class Header extends React.Component{
         );
     }
 }
+
+*/
