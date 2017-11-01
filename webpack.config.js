df75253135b2aa82811fbd7dfe5be9bd3dc5297e
Nitesh <niteshp27@gmail.com>
2017-11-01 15:21:19 +0530

    enabled source maps for js and css

--- webpack.config.js
+++ webpack.config.js
@@ -8,23 +8,18 @@ const env = process.env.NODE_ENV
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-var config = {
-    entry: SRC_DIR + "/app/index.js",
+const config = {
+    entry: {
+        index: SRC_DIR + "/app/index.js"
+    },
     output: {
         path: DIST_DIR + "/app",
         filename: "bundle.js",
-        publicPath: "/app"
+        publicPath: "/app",
     },
+    devtool: 'source-map',   //builds sourcemap for files
     module:{
-        loaders: [
-            {
-                test: /\.js?/,
-                include: SRC_DIR,
-                loader: "babel-loader",
-                query: {
-                    presets: ["react", "es2015", "stage-2"]
-                }
-            },
+        loaders: [            
             {
                 test: /\.css$/,
                 use: env === 'production'
@@ -36,18 +31,24 @@ var config = {
                     {
                         loader: 'style-loader',
                         options: {
-                            sourceMap: false
+                            sourceMap: true
                         }                            
                     },
                     {
                         loader: 'css-loader',
                         options: {
-                            sourceMap: false
+                            sourceMap: true
                         }
                     }
                     //'style-loader', 'css-loader' //can be used without sourcemaps
                 ]
             },
+            // Optionally extract less files
+            // or any other compile-to-css language
+            // {
+            //     test: /\.less$/,
+            //     loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            // },
             {
                 test: /\.scss$/,
                 use: env === 'production'
@@ -59,32 +60,71 @@ var config = {
                     {
                         loader: 'style-loader',
                         options: {
-                            sourceMap: false
+                            sourceMap: true
                         }                            
                     },
                     {
                         loader: 'css-loader',
                         options: {
-                            sourceMap: false
+                            sourceMap: true
                         }
                     }
-                    //'style-loader', 'css-loader' //can be used without sourcemaps
+                    //'style-loader', 'css-loader' // can be used without sourcemaps
                  ]
             },
             {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            },
+            {
+                    test: /\.tsx?$/,
+                    loader: "ts"                       
+            },
+            {
                 test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                include: SRC_DIR,
                 loader: 'url-loader',
                 options: {
                   limit: 10000
                 }
-            }
-            
+            }         
         ]
+        // ,
+        // rules:[
+        //     {
+        //         test: /\.(png|jpg|gif)$/,
+        //         use: [
+        //             {
+        //                 loader: 'file-loader',
+        //                 options: {
+        //                     name: '[path][name].[ext]',
+        //                     outputPath: 'images/'
+        //                 }  
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         test: /\.(svg|eot|ttf|woff|woff2)$/,
+        //         use: [
+        //             {
+        //                 loader: 'file-loader',
+        //                 options: {
+        //                 name: '[path][name].[ext]',
+        //                 outputPath: 'fonts/'
+        //                 }  
+        //             }
+        //         ]
+        //     }
+        // ]
     },
     plugins: env === 'production'
     ? [
         new ExtractTextPlugin({
-          filename: 'styles.css'
+          filename: 'appstyles.css'
         })
       ]
     : []
